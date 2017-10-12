e5ab578554b5c79040f57702ff68f5e7893505d9
Fernando Diaz Laclaustra <f.diazlaclaustra@umlaud.es>
2017-10-12 12:32:05 +0200

    Implementa clase principal LogentriesStream basado en hologger-stream.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-module.exports = {};
+/**
+ * Module dependencies.
+ */
+var LogentriesStreamer = require('./lib/logentries-streamer');
+
+
+/**
+ * Expose `LogentriesStreamer` directly from package.
+ */
+exports = module.exports = LogentriesStreamer;
